9f639eda477d687690a7f591f6a06bba54b2f1c4
ZirvedaAytimur <zirvedaytimur@gmail.com>
2019-08-24 13:10:31 +0300

    Using global styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Ke
 import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
+import BodyText from '../components/BodyText';
 import Colors from '../constants/colors';
+import TitleText from '../components/TitleText';
 
 const StartGameScreen = props => {
 
@@ -38,9 +40,9 @@ const StartGameScreen = props => {
     if (confirmed) {
         confirmedOutput = (
             <Card style={styles.summaryContainer}>
-                <Text>You selected</Text>
+                <BodyText>You selected</BodyText>
                 <NumberContainer>{selectedNumber}</NumberContainer>
-                <Button title="START GAME" onPress={()=>props.onStartGame(selectedNumber)}/>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
             </Card>
         );
     }
@@ -50,9 +52,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss();
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
                     <View style={styles.buttonContainer}>
                         <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Colors.accent} /></View>
@@ -74,7 +76,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
-        fontFamily:'open-sans-bold'
+        fontFamily: 'open-sans-bold'
     },
     inputContainer: {
         width: 300,
